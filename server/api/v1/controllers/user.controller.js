dad9271c37f2993133e758497c216035b12f96b6
hnefatafl01 <kevin.erick01@gmail.com>
2017-12-11 11:00:55 -0700

    delete user

--- server/api/v1/controllers/user.controller.js
+++ server/api/v1/controllers/user.controller.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const User = require('../models/user.model');
 const status = {
     BadReq: { message: "Bad Request: Could not retrieve users"},
-    ServerError: { message: "Server Error: Could not create user"}
+    DeleteError: { message: "ServerError: Could not delete user"},
+    CreateError: { message: "ServerError: Could not delete user"},
 }
-
 
 module.exports = {
     getUsers: function(req, res) {
@@ -20,7 +20,7 @@ module.exports = {
         return User.create({ username: req.body.username, password: req.body.password }, (err, user) => {
             if (err) { 
                 throw new Error(err);
-                res.status(500).send(status.ServerError);
+                res.status(500).send(status.CreateError);
             }
             res.send(user);
         });
@@ -45,6 +45,15 @@ module.exports = {
                     res.status(400).send(status.BadReq);
                 }
                 res.send(user);
+        });
+    },
+    deleteUser: function(req, res) {
+        return User.findByIdAndRemove({ _id: req.params.id }, (err, result) => {
+            if (err) {
+                throw new Error(err);
+                res.status(500).send(status.DeleteError);
+            }
+            res.send("Deleted user with ID: " + result._id);
         });
     }
     
--- server/api/v1/user.routes.js
+++ server/api/v1/user.routes.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const User = require('./models/user.model');
 const UserQ = require('./controllers/user.controller');
 
-router.put('/:id', UserQ.updateUser);
+router.delete('/:id/delete', UserQ.deleteUser);
+router.put('/:id/edit', UserQ.updateUser);
 router.get('/:id', UserQ.getUser);
 router.get('/', UserQ.getUsers);
 router.post('/', UserQ.createUser);
-
 
 module.exports = router;
